fc8fae28a10990db45e5476582f718edbb6d23b9
Alex Ballera <alexballera@gmail.com>
2020-11-13 12:24:07 -0300

    Agregado checkIsAuthenticated

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanActivateChild, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, CanActivateChild, CanLoad, Route, Router, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '@auth0/auth0-angular';
 
@@ -7,20 +7,31 @@ import { AuthService } from '@auth0/auth0-angular';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
-  constructor(public auth: AuthService) {}
+  constructor(public auth: AuthService, private router: Router) {
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      return this.auth.isAuthenticated$;
+      return this.checkIsAuthenticated();
   }
   canActivateChild(
     childRoute: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      return this.auth.isAuthenticated$;
+      return this.checkIsAuthenticated();
   }
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      return this.auth.isAuthenticated$;
+      return this.checkIsAuthenticated();
+  }
+
+  checkIsAuthenticated(): boolean {
+    let valor = false;
+    this.auth.isAuthenticated$.subscribe(val => valor = val).unsubscribe();
+    if (!valor) {
+      this.router.navigate(['home']);
+      return false;
+    }
+    return true;
   }
 }
