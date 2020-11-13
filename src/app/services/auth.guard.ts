c7054462bc79d4a8fc11a7fa6d19eb62bf734378
Alex Ballera <alexballera@gmail.com>
2020-11-13 15:26:46 -0300

    cambiado value

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -10,16 +10,28 @@ export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
   constructor(private auth: AuthService,
               private router: Router) {
   }
+  // Permite a un usuario entrar a una ruta específica
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       return this.checkIsAuthenticated();
   }
+  // Permite a un usuario entrar a una ruta específica hija
   canActivateChild(
     childRoute: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       return this.checkIsAuthenticated();
   }
+  // Determia si un usuario puede salir de una ruta específica. p.e un formulario si no ha guardado los cambios
+  canDeactivate(
+    component: unknown,
+    currentRoute: ActivatedRouteSnapshot,
+    currentState: RouterStateSnapshot,
+    nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+  // Permite cargar un módulo de característica asíncrona Lazy Loading
+  // Resolve: obtiene la data que el componente va a mostrar
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
